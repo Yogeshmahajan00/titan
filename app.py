7707d0ace533d25d04455b653070b52ac0a56690
Yogeshmahajan00 <yogeshmahajan085@gmail.com>
2023-03-23 18:48:48 +0530

    ADDED

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask,request,render_template
+import pickle
+import numpy as np
+model=pickle.load(open('model.pkl','rb'))
+app=Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/predict",methods=['POST'])
+def survival():
+    Pclass=int(request.form.get("Pclass",False))
+    Sex=int(request.form.get("Sex",False))
+    Age=int(request.form.get("Age",False))
+    SibSp=int(request.form.get("SibSp",False))
+    Parch=int(request.form.get("Parch",False))
+    Fare=int(request.form.get("Fare",False))
+    Embarked=int(request.form.get("Embarked",False))
+
+    result=model.predict(np.array([Pclass,Sex,Age,SibSp,Parch,Fare,Embarked]).reshape(1,7))
+    if result[0]==1:
+        return "<h1 style='color:green'>Survived</h1>"
+    else:
+        return "<h1 style='color:red'>NOT Survived</h1>"
+    
+app.run(debug=True)
